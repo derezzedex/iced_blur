7f45a2937aed50e97c742a2b23a7383fbf77f400
Richard Custodio <richardcustodio@pm.me>
2025-04-28 18:21:43 -0300

    setup widget impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,118 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use iced_core::widget::tree::{self, Tree};
+use iced_core::{
+    Clipboard, Element, Event, Layout, Length, Rectangle, Shell, Size, Vector, Widget,
+};
+use iced_core::{layout, mouse, overlay, renderer, widget};
+
+pub struct Blur<'a, Message, Theme, Renderer> {
+    content: Element<'a, Message, Theme, Renderer>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a, Message, Theme, Renderer> Widget<Message, Theme, Renderer>
+    for Blur<'a, Message, Theme, Renderer>
+where
+    Renderer: iced_core::Renderer + iced_core::renderer::Headless,
+    Theme: iced_core::theme::Base,
+{
+    fn size_hint(&self) -> Size<Length> {
+        self.content.as_widget().size_hint()
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn size(&self) -> Size<Length> {
+        self.content.as_widget().size()
+    }
+
+    fn tag(&self) -> tree::Tag {
+        self.content.as_widget().tag()
+    }
+
+    fn state(&self) -> tree::State {
+        self.content.as_widget().state()
+    }
+
+    fn children(&self) -> Vec<Tree> {
+        self.content.as_widget().children()
+    }
+
+    fn diff(&self, tree: &mut Tree) {
+        self.content.as_widget().diff(tree);
+    }
+
+    fn layout(
+        &self,
+        tree: &mut Tree,
+        renderer: &Renderer,
+        limits: &layout::Limits,
+    ) -> layout::Node {
+        self.content.as_widget().layout(tree, renderer, limits)
+    }
+
+    fn operate(
+        &self,
+        state: &mut Tree,
+        layout: Layout<'_>,
+        renderer: &Renderer,
+        operation: &mut dyn widget::Operation,
+    ) {
+        self.content
+            .as_widget()
+            .operate(state, layout, renderer, operation);
+    }
+
+    fn update(
+        &mut self,
+        state: &mut Tree,
+        event: &Event,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        renderer: &Renderer,
+        clipboard: &mut dyn Clipboard,
+        shell: &mut Shell<'_, Message>,
+        viewport: &Rectangle,
+    ) {
+        self.content.as_widget_mut().update(
+            state, event, layout, cursor, renderer, clipboard, shell, viewport,
+        );
+    }
+
+    fn draw(
+        &self,
+        tree: &Tree,
+        renderer: &mut Renderer,
+        theme: &Theme,
+        style: &renderer::Style,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        viewport: &Rectangle,
+    ) {
+        self.content
+            .as_widget()
+            .draw(tree, renderer, theme, style, layout, cursor, viewport);
+    }
+
+    fn mouse_interaction(
+        &self,
+        state: &Tree,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        viewport: &Rectangle,
+        renderer: &Renderer,
+    ) -> mouse::Interaction {
+        self.content
+            .as_widget()
+            .mouse_interaction(state, layout, cursor, viewport, renderer)
+    }
+
+    fn overlay<'b>(
+        &'b mut self,
+        state: &'b mut Tree,
+        layout: Layout<'_>,
+        renderer: &Renderer,
+        viewport: &Rectangle,
+        translation: Vector,
+    ) -> Option<overlay::Element<'b, Message, Theme, Renderer>> {
+        self.content
+            .as_widget_mut()
+            .overlay(state, layout, renderer, viewport, translation)
     }
 }
